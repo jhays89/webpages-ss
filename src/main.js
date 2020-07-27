b522ac8bf246f7c943a983d896e7856f9e94f24d
Justin Hays <jhays478@gmail.com>
2020-07-27 13:01:14 +0000

    initiated rackspace upload

--- src/main.js
+++ src/main.js
@@ -1,15 +1,39 @@
+
+const fs = require('fs');
 const xlsx = require('node-xlsx');
 const puppeteer = require('puppeteer');
+const pkgcloud = require('pkgcloud');
+const appSettings = require('../appsettings.js');
 
-const main = (args) => {
+async function  main(args) {
   //console.log(`Hello, ${args.name}, ${args.title}`);
   // Will need to validate args => ValidateArgs(args) == use try-catch return error descirbing wtf is wrong
   // check if webpages.ext exists. if not, return error message stating what user needs to do
   var file = xlsx.parse('sample.xlsx');
   const rows = file[0].data;
   const urls = getUrlsFromRows(rows);
-  getScreenShots(urls);
+  const binaryFile = await getBinaryFile(urls);
 
+  const client = pkgcloud.storage.createClient(appSettings.rackspace);
+  var source = fs.createReadStream(binaryFile);
+  var dest = client.upload({
+    container: 'sample-container-test',
+    remote: 'somefile.txt'
+  });
+  
+  dest.on('error', function(err) {
+    // TODO handle err as appropriate
+  });
+  
+  dest.on('success', function(file) {
+    // TODO handle successful upload case
+  });
+  
+  // pipe the source to the destination
+  
+  //START HERE https://github.com/pkgcloud/pkgcloud/blob/HEAD/docs/providers/rackspace/storage.md#clientuploadoptions
+  source.pipe(dest);
+  
   // parse webpages.ext
   // run puppeteer on URLS
   // save images to rackspace
@@ -27,15 +51,17 @@ const getUrlsFromRows = (rows) => {
   return urls;
 };
 
-async function getScreenShots(urls) {
+async function getBinaryFile(urls) {
   var url = urls[0];
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  const base64String = await page.screenshot({fullPage: true, encoding: 'base64'});
-  
+  const binaryFile = await page.screenshot({fullPage: true, encoding: 'base64'});
+
   await browser.close();
+
+  return binaryFile;
 };
 
 module.exports = main;
