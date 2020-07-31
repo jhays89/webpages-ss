cfc03b4b3be9ad8865ac92b5661a77ddf76a04a4
Justin Hays <jhays478@gmail.com>
2020-07-31 13:05:28 +0000

    able to get url of freshly uploaded file

--- src/main.js
+++ src/main.js
@@ -6,77 +6,41 @@ const pkgcloud = require('pkgcloud');
 const appSettings = require('../appsettings.js');
 
 async function main(args) {
-  //console.log(`Hello, ${args.name}, ${args.title}`);
-  // Will need to validate args => ValidateArgs(args) == use try-catch return error descirbing wtf is wrong
-  // check if webpages.ext exists. if not, return error message stating what user needs to do
-  var file = xlsx.parse('sample.xlsx');
+  const file = xlsx.parse('sample.xlsx');
   const rows = file[0].data;
   const urls = getUrlsFromRows(rows);
+  const url = appSettings.rackspace.urlPrefix;
 
+  const uploadOptions = {
+    container: 'webpages',
+    remote: 'webpages-' + new Date().getTime(),
+  };
+
+  // TODO loop over urls
+  // Get url
+  // Update appropriate 
+  
   try {
-
     await GetScreenShot(urls);
 
     const client = pkgcloud.storage.createClient(appSettings.rackspace);
 
-    // var fileName = 'example-' + new Date().toISOString();
-    // var contentType = 'image/png';
-    //const buff = Buffer.from(base64EncodedFile, 'base64');
-    //var size = fs.statSync('screenshot.png')['size'];
+    const readStream = fs.createReadStream('screenshot.jpeg');
 
-    const readStream = fs.createReadStream('screenshot.jpeg'); // issue is probably here - try using bytes instead. It doesn't like the base64
-
-    // const options = {
-    //   container: 'webpages',
-    //   remote: fileName,
-    //   contentType: contentType,
-    //   size: size
-    // };
+    const writeStream = client.upload(uploadOptions);
     
-    const options = {
-      container: 'webpages',
-      remote: 'webpages-' + new Date().getTime(),
-    };
-    
-    const writeStream = client.upload(options);
-    
-    writeStream.on('error', function(err) {
-      removeScreenShot();
+    writeStream.on('error', err =>  {
       console.log(err)
     });
     
-    writeStream.on('success', function(file) {
-      removeScreenShot();
-      // need to get name from the callback and then go fetch it for the url??? sucks ass man
-      const test = file;
+    writeStream.on('success', response => {
+      url += response.name;
     });
-
-    writeStream.on('unpipe', function(file) {
-      removeScreenShot();
-      console.log("unpipe");
-    });
-
-    writeStream.on('end', function(file) {
-      removeScreenShot();
-      console.log("end");
-    });
-
-    // writeStream.on('data', function(file) {
-    //   removeScreenShot();
-    //   console.log("data");
-    // });
     
-    const rackspaceUrl = readStream.pipe(writeStream);
-
-    const test = "dd";
-    
-    // parse webpages.ext
-    // run puppeteer on URLS
-    // save images to rackspace
-    // update webpages.ext with rackpace URLS
+    readStream.pipe(writeStream);
   }
   catch(err) {
-    const test = err;
+    console.log(err);
   }
 };
 
